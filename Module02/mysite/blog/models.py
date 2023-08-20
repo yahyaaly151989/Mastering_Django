fa70c44b72ddaafe593100771b3662d01d523dea
Yehya Ali <yahyaaly151989@gmail.com>
2023-08-20 19:56:46 +0800

    Added canonical URLs for models

--- Module02/mysite/blog/models.py
+++ Module02/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     
@@ -25,3 +26,9 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[
+            self.id
+        ])
+    
