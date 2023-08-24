c0dcdd1a6176521090348bf13f4a06efb6da3f92
Yehya Ali <yahyaaly151989@gmail.com>
2023-08-25 01:12:53 +0800

    Building complex QuerySets to recommend similar posts

--- Module03/mysite/blog/views.py
+++ Module03/mysite/blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     post_list = Post.objects.all()
@@ -46,7 +47,12 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     # Retrieve post by id
