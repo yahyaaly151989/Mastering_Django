faf201463f07215f9ad506bcd77d1ccc4ae74e38
Yehya Ali <yahyaaly151989@gmail.com>
2023-09-18 11:40:41 +0800

    Using the Django authentication framework - Login and Logout

--- Module04/bookmarks/account/urls.py
+++ Module04/bookmarks/account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('login', views.user_login, name='login'),
+    # path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- Module04/bookmarks/bookmarks/settings.py
+++ Module04/bookmarks/bookmarks/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
