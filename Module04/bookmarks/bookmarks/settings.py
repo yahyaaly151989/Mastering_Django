736a110d527cdf8e8eb684b519e52a1ee9333636
Yehya Ali <yahyaaly151989@gmail.com>
2023-09-20 23:11:03 +0800

    password change, and password reset

--- Module04/bookmarks/bookmarks/settings.py
+++ Module04/bookmarks/bookmarks/settings.py
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
