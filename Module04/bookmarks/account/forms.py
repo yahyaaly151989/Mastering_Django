78c8f5a1c4de8b2ec51e23663ea91c8334c28d37
Yehya Ali <yahyaaly151989@gmail.com>
2023-09-24 00:14:45 +0800

    Preventing users from using an existing email

--- Module04/bookmarks/account/forms.py
+++ Module04/bookmarks/account/forms.py
@@ -23,11 +23,25 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
     
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already exists.')
+        return data
+        
+    
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
         
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Email already exists.')
+        return data
+    
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
